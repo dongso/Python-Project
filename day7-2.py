c124d3d4dc29484288eb85a150ea9b26d786733e
hayoungishere <48154977+hayoungishere@users.noreply.github.com>
2019-04-05 16:33:20 +0900

    day7-2

---
+++ day7-2.py
@@ -0,0 +1,154 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+from urllib.parse import urljoin
+
+import requests
+
+# soupt.select_one(선택자):선택자 하나를 추출
+# soup.select(선택자) : 선택자에 해당하는 여러 개의 요소를 리스트 형태로 추출
+
+html="""
+<html><body>
+    <div id="book">
+    <h1>어린왕자</h1>
+    <ul class="items">
+    <li>생떽쥐베리</li>
+    <li>사막 불시창</li>
+    <li>소년과 양</li>
+    </ul>
+    </div>
+</body></html>
+"""
+
+#html 분석
+soup=BeautifulSoup(html,'html.parser')
+h1=soup.select_one("div#book > h1").string #작성형식 : 태그명#아이디 > 하위태그.클래스명 >...
+print("제목=",h1)
+
+#목록 추출
+li_list=soup.select("div#book > ul.items > li") #.string -> ERROR ::list이기 때문에 for문을 사용해서 .string 적용할 것.
+for li in li_list:
+    print("목록=", li.string)
+
+
+url="http://finance.naver.com/marketindex/"
+res=req.urlopen(url)
+soup=BeautifulSoup(res,'html.parser')
+#print(soup)
+
+usd=soup.select("div.head_info > span.value")
+print("usd/kw=",usd)
+
+
+myList=soup.select("a.head.eur> div > span.value")
+print(myList)
+
+#exchangeList > li:nth-child(1) > a.head.usd > div > span.value
+#exchangeList > li.on > a.head.eur > div > span.value
+
+url="https://ko.wikisource.org/wiki/%EC%A0%80%EC%9E%90:%EC%9C%A4%EB%8F%99%EC%A3%BC"
+res=req.urlopen(url)
+soup=BeautifulSoup(res,'html.parser')
+bookTitle=soup.select_one("#mw-content-text > div > ul:nth-child(6) > li > b > a")
+print(bookTitle.string)
+
+s1=soup.select_one("#mw-content-text > div > ul:nth-child(6) > li > ul > li:nth-child(1) > a").string
+print(s1)
+
+s2=soup.select_one("#mw-content-text > div > ul:nth-child(6) > li > ul > li:nth-child(2) > a").string
+print(s2)
+
+myList=soup.select("#mw-content-text > div > ul:nth-child(6) > li > ul > li> a")
+for mL in myList:
+    print(mL.string)
+
+
+url=("https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=%EB%B8%94%EB%9E%99%ED%95%91%ED%81%AC")
+res=req.urlopen(url)
+soup=BeautifulSoup(res,'html.parser')
+kinTitle=soup.select_one("#elThumbnailResultArea > li:nth-child(4) > dl > dt > a").string
+#print(kinTitle)
+
+
+print("*"*10+"HOME.html에서 꺼삐딴리 추출하기"+"*"*10)
+#HOME.html에서 꺼삐딴리 추출하기
+# id="title"
+# id="id4"
+
+with open('HOME.html','r',encoding='utf-8') as fp:
+    soup=BeautifulSoup(fp,'html.parser')
+    #print(soup)
+    sel=lambda q:print(soup.select_one(q).string)
+    sel("#id4")
+    sel("li#id4")
+    sel("ul > li#id4")
+    sel("#title > li#id4")
+    sel("#title #id4") #id안에 id
+    sel("#title > #id4") #id > id
+    sel("ul#title > li#id4")
+    sel("li[id='id4']")
+    sel("li:nth-of-type(4)")
+    print(soup.select("li")[3].string)
+    print(soup.find_all("li")[3].string)
+
+
+
+#로그인이 필요한 사이트로부터 스크래핑
+
+#로그인 => http://www.hanbit.co.kr/member/login.html
+url="http://www.hanbit.co.kr/member/login.html"
+res=req.urlopen(url)
+soup=BeautifulSoup(res,'html.parser')
+
+#아이디 => #m_id
+#비밀번호 => #m_passwd
+#마이한빛 => http://www.hanbit.co.kr/myhanbit/myhanbit.html
+#마일리지 => #container > div > div.sm_mymileage > dl.mileage_section1 > dd > span
+#코인 => #container > div > div.sm_mymileage > dl.mileage_section2 > dd > span
+#로그인확인 => http://www.hanbit.co.kr/member/login_proc.php
+#http://www.hanbit.co.kr/myhanbit/myhanbit.html <- 코인추출
+#index.html > login_proc.php > index.html > myhanbit.html
+
+
+USER=""
+PASSWORD=""
+
+#Server Connection
+session=requests.session() #Session Object : 웹서버에 접속할 수 있도록 해주는 객체
+
+login_info={ #로그인 정보를 딕셔너리 형태로 만들어서 post()를 통해 전달
+    "m_id":USER,
+    "m_passwd":PASSWORD
+}
+url_login="http://www.hanbit.co.kr/member/login_proc.php"
+res=session.post(url_login,login_info)
+
+url_mypage="http://www.hanbit.co.kr/myhanbit/myhanbit.html"
+res=session.get(url_mypage)
+
+soup=BeautifulSoup(res.text,'html.parser')
+mileage=soup.select_one("#container > div > div.sm_mymileage > dl.mileage_section1 > dd > span").string
+ecoin=soup.select_one("#container > div > div.sm_mymileage > dl.mileage_section2 > dd > span").string
+print("mileage-> ",mileage," \nE coin->",ecoin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- day7.py
+++ day7.py
@@ -6,6 +6,99 @@
 
 #import : 모듈 불러오기
 
-import module1
+from bs4 import BeautifulSoup
+import urllib.request as req
+html="""
+<html><body>
+<h1> 스크래핑</h1>
+<p> 웹 페이지 분석 </p>
+<p>부분 추출</p>
+<h1>next</h1>
+</body></html>
+"""
 
-print(module1.add(5,6))
+#html 문서 분석
+#BeautifulSoupt(분석할문서객체, parser)
+soup=BeautifulSoup(html,'html.parser')
+#BeautifulSoup : html, xml 문서를 분석할 수 있는 다양한 함수들이 제공됨
+#생성자함수(클래스이름과 동일) : 객체를 생성할 때 호출하는 특별한 의미의 함수
+#셍성자 함수를 호출해야 객체가 생성됨.
+#만들어진 객체를 통해 클래스가 가지고 있던 함수나 변수 등을 사용할 수 있게 됨.
+
+v1=soup.html.body.h1
+print(v1)
+print(v1.string) #<h1>태그에 포함된 순수 텍스트만 출력됨
+
+#태그 이름을 점(.)으로 구분하여 나열
+
+vp= soup.html.body.p
+print(vp.string)
+vp=vp.next_sibling #줄바꿈 문자가 들어가게 됨
+vp=vp.next_sibling #따라서, next_sibling을 두번 해줘야됨
+print(vp.string)
+
+v4=soup.h1 #html.body ...이 아니고 바로 접근해도 됨.
+print(v4.string)
+
+print("*"*50)
+v5=soup.p
+print(v5.string)
+v6=v5.next_sibling.next_sibling
+print(v6.sting)
+
+print("="*100)
+html="""
+<html><body>
+<h1 id="title"> 스크래핑</h1>
+<p id="mybody"> 웹 페이지 분석 </p>
+<p id="mybody2">부분 추출</p>
+</body></html>
+"""
+#find()::id 속성의 값을 통해 데이터를 추출하는 함수
+soup=BeautifulSoup(html,'html.parser')
+res=soup.find(id="title")
+res1=soup.find(id="mybody")
+res2=soup.find(id="mybody2")
+print(res.string, res1.string, res2.string)
+
+print("="*100)
+html="""
+<html><body>
+<ul> <!-- unordered list-->
+<li><a href="http://www.naver.com">naver</a></li>
+<li><a href="http://www.daum.net">daum</a></li>
+</ul>
+</body></html>
+"""
+#Attribute의 값을 추출해보자
+soup=BeautifulSoup(html,'html.parser')
+links=soup.find_all("a") # 태그검색 출력결과 -> list
+print(links)
+#find: 한 개를 찾을때
+#finde_all : 태그에 해당하는 전체 데이터를 가져올때
+for i in links:
+    href=i.attrs['href']
+    text=i.string
+    print(text,'==>',href)
+
+
+
+
+
+#HTML : 비구조적=> 해독불가능 / XML: 구조적=> 해독가능
+#mw-content-text > div > ul:nth-child(6) > li
+
+print("="*100)
+
+import urllib.request as req
+url='http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
+res=req.urlopen(url)
+print(res)
+soup=BeautifulSoup(res,"html.parser") #httpResponse 객체를 html.parser를 사용해서 불러옴
+title=soup.find("title")
+print(title.string)
+
+print("="*100)
+
+
+
